190dbf0166a4d2939b926736990203c69f668ab6
Paul <paulik0182@gmail.com>
2022-10-04 13:58:00 +0300

    Создали RootFragment (возможно для главного экрана)

---
+++ app/src/main/java/com/example/pressurediary/ui/root/RootFragment.kt
@@ -0,0 +1,23 @@
+package com.example.pressurediary.ui.root
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pressurediary.R
+
+class RootFragment : Fragment(R.layout.fragment_root) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView(view)
+    }
+
+    private fun initView(view: View) {
+        TODO("Not yet implemented")
+    }
+
+
+}
