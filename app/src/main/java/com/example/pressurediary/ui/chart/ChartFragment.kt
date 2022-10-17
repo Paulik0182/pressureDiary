b03d6b6ecfd2142ca91e649800dbd4fda7ff402c
Paul <paulik0182@gmail.com>
2022-10-17 16:26:42 +0300

    Настроили ГРАФИК pieChart (Данные из REPO. Вариант 1 - подробный)

--- app/src/main/java/com/example/pressurediary/ui/chart/ChartFragment.kt
+++ app/src/main/java/com/example/pressurediary/ui/chart/ChartFragment.kt
@@ -6,51 +6,91 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.pressurediary.R
+import com.example.pressurediary.domain.BpEvaluation
+import com.example.pressurediary.domain.interactors.BpDaoInteractor
+import com.example.pressurediary.domain.interactors.BpEvaluator
+import com.example.pressurediary.domain.repos.BpRepo
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import org.koin.android.ext.android.inject
+import org.koin.core.component.getScopeName
 
 class ChartFragment : Fragment(R.layout.fragment_chart) {
 
     private lateinit var pieChart: PieChart
+
+    private val bpRepo: BpRepo by inject() //получили через Koin
+    private val bpEvaluator: BpEvaluator by inject() //получили enum значения через Koin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         pieChart = view.findViewById(R.id.pie_chart)
 
+        val bpList = bpRepo.getAllBpList()//получили данные
+
+        // делаем списки с состояниями (у нас есть типы оценки состояния) завели Счетчик
+        var normalBpCount = 0
+        var preHypertensionBpCount = 0
+        var hypertension1BpCount = 0
+        var hypertension2BpCount = 0
+        var unknownBpCount = 0
+
+        // заполняем данными (прошлись по всем, в зависимости от оценки поставили оценку)
+        bpList.forEach {
+            when (bpEvaluator.evaluate(it)) {
+                BpEvaluation.NORMAL -> normalBpCount++
+                BpEvaluation.PRE_HYPERTENSION -> preHypertensionBpCount++
+                BpEvaluation.HYPERTENSION_1 -> hypertension1BpCount++
+                BpEvaluation.HYPERTENSION_2 -> hypertension2BpCount++
+                BpEvaluation.UNKNOWN -> unknownBpCount++
+            }
+        }
+
         //Заводим некий лист входных псевдо данных
         val pieEntries = listOf<PieEntry>(
-            PieEntry(100f, "Высокое"),
-            PieEntry(50f, "Низкое"),
-            PieEntry(10f, "Нормальное")
+            PieEntry(normalBpCount.toFloat(), "Нормальное"),
+            PieEntry(preHypertensionBpCount.toFloat(), "Предгипертензия"),
+            PieEntry(hypertension1BpCount.toFloat(), "Гипертензия 1"),
+            PieEntry(hypertension2BpCount.toFloat(), "Гипертензия 2"),
+            PieEntry(unknownBpCount.toFloat(), "Не известно 1")
         )
 
         //получаем количество значений и название
         val pieDataSet = PieDataSet(pieEntries, "Давление")
 
-            //Раскрашиваем значения. ВНИМАНИЕ на написание colors
-        pieDataSet.colors = listOf(Color.CYAN, Color.RED, Color.GREEN)
+        //Раскрашиваем значения. ВНИМАНИЕ на написание colors
+        pieDataSet.colors = listOf(
+            Color.GREEN,
+            Color.CYAN,
+            Color.YELLOW,
+            Color.RED,
+            Color.GRAY
+        )
 
         //заводим сущьность pie data
         val pieData = PieData(pieDataSet)
+        pieData.setValueTextSize(16f)//задали размер выведеных значений
 
         //принимает на вход некую сущьность (данные)
         pieChart.data = pieData
 
-            //регулирования радиуса отверстия
+        //регулирования радиуса отверстия
 //        pieChart.holeRadius = 0f
         pieChart.isDrawHoleEnabled = false // Убрать отверстие
 //        pieChart.setCenterTextColor(Color.BLACK)//цвет значений (не названий)
         pieChart.setEntryLabelColor(Color.BLACK)//цвет всех надписей
-        pieChart.setEntryLabelTextSize(22f)//размер только надписей
+        pieChart.setDrawEntryLabels(false)//отключили надписи
+//        pieChart.setEntryLabelTextSize(22f)//размер только надписей
+        pieChart.setUsePercentValues(true)//Использовать процентные соотношения %
 
         //Условное название графика
         pieChart.description.text = "Расспределение давления"
         pieChart.description.textSize = 16f//размер названия
 
-            //Цвет фона
+        //Цвет фона
         pieChart.setBackgroundColor(Color.WHITE)
     }
 
