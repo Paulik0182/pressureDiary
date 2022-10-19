b298d86d38e165680111ef395a6fd3f276754c70
Paul <paulik0182@gmail.com>
2022-10-19 10:11:47 +0300

    Correction cod

--- app/src/main/java/com/example/pressurediary/domain/BpEvaluation.kt
+++ app/src/main/java/com/example/pressurediary/domain/BpEvaluation.kt
@@ -1,8 +1,9 @@
 package com.example.pressurediary.domain
 
 import android.graphics.Color
+import androidx.annotation.ColorInt
 
-enum class BpEvaluation(val color: Int) {
+enum class BpEvaluation(@ColorInt val color: Int) {
     NORMAL(Color.GREEN),
     PRE_HYPERTENSION(Color.BLUE),
     HYPERTENSION_1(Color.RED),
