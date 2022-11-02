74e7c9e62e53461672364134a321dfadb55c9bfb
Paul <paulik0182@gmail.com>
2022-11-02 17:00:16 +0300

    Создали json файл для того чтобы положить в firebase

--- app/src/main/java/com/example/pressurediary/domain/entities/BpEntity.kt
+++ app/src/main/java/com/example/pressurediary/domain/entities/BpEntity.kt
@@ -2,8 +2,10 @@ package com.example.pressurediary.domain.entities
 
 import android.os.Parcelable
 import com.example.pressurediary.domain.Emoji
+import com.google.firebase.database.PropertyName
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 /**
  * Уневерсальный способ представление времени. Время считают (хранят) в Long
@@ -13,14 +15,25 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class BpEntity(
     var id: Long = 0,
-    var timeInMs: Long = 0,
+    @get: PropertyName("time_in_ms")
+    @field: PropertyName("time_in_ms")
+    @SerializedName("time_in_ms")
+    var timeInMs: Long = Calendar.getInstance().timeInMillis,
+    @get: PropertyName("upper_level")
+    @field: PropertyName("upper_level")
     @SerializedName("upper_level")
     var systolicLevel: Int = 120,
+    @get: PropertyName("lower_level")
+    @field: PropertyName("lower_level")
     @SerializedName("lower_level")
     var diastolicLevel: Int = 80,
     var pulse: Int = 60,
+    @get: PropertyName("well_being")
+    @field: PropertyName("well_being")
     @SerializedName("well_being")
     var wellBeing: Emoji = Emoji.FINE,
+    @get: PropertyName("condition_user")
+    @field: PropertyName("condition_user")
     @SerializedName("condition_user")
     var conditionUser: String = "Хорошее"
 ) : Parcelable
