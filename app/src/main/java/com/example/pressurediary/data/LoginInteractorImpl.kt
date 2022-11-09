18a95b7f63726f3799b130ef7655e29e1133f488
Paul <paulik0182@gmail.com>
2022-11-09 22:09:40 +0300

    авторизация пользователя (небольшая надстройка)

--- app/src/main/java/com/example/pressurediary/data/LoginInteractorImpl.kt
+++ app/src/main/java/com/example/pressurediary/data/LoginInteractorImpl.kt
@@ -6,9 +6,9 @@ import com.example.pressurediary.domain.interactors.LoginInteractor
 class LoginInteractorImpl : LoginInteractor {
 
     //для примера создаем пользователя и возвращаем его
-    private val mockUser: UserEntity = UserEntity(
-        name = "mockUser"
-    )
+//    private val mockUser: UserEntity = UserEntity(
+//        name = "mockUser"
+//    )
 
     override fun login(login: String, password: Int, onResult: (UserEntity?) -> Unit) {
         //проверка пользователя
@@ -18,7 +18,7 @@ class LoginInteractorImpl : LoginInteractor {
             onResult.invoke(null)
         } else {
 //            onResult.invoke(null)
-            onResult.invoke(UserEntity(name = login))
+            onResult.invoke(UserEntity(id = login, name = login))
         }
     }
 }
